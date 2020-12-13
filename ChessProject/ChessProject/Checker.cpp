cb18bd0d27130c45822ee6ffd09d6ebec539e4cd
LastAirBender <roie.zemel@leyada.net>
2020-12-13 16:43:21 +0200

    Added Checker.h and Checker.cpp

---
+++ ChessProject/ChessProject/Checker.cpp
@@ -0,0 +1,37 @@
+#include "Checker.h"
+
+/*
+    Get x.
+    Input: none.
+    Output: x.
+*/
+int Checker::getX() {
+    return x;
+}
+
+/*
+    Get y.
+    Input: none.
+    Output: y.
+*/
+int Checker::getY() {
+    return y;
+}
+
+/*
+    Set x.
+    Input: x.
+    Output: none.
+*/
+void Checker::setX(int x) {
+    this->x = x;
+}
+
+/*
+    Set y.
+    Input: y.
+    Output: none.
+*/
+void Checker::setY(int y) {
+    this->y = y;
+}
---
+++ ChessProject/ChessProject/Checker.h
@@ -0,0 +1,14 @@
+#pragma once
+class Checker
+{
+
+private:
+	int x;
+	int y;
+public:
+	int getX();
+	int getY();
+	void setX(int x);
+	void setY(int y);
+};
+
